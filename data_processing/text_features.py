596f2fb317809d70ee62f378e1f38e577c6d803d
stefan-kohnen <stefan.kohnen@rwth-aachen.de>
2021-04-28 10:54:55 +0200

    Use BERT upsampling method from Gesticulator repo.

--- data_processing/text_features.py
+++ data_processing/text_features.py
@@ -25,8 +25,9 @@ def create_embedding(name):
 def extract_bert_features(textpath, files, destpath, fps):
     embedding_model = create_embedding("BERT")
     fps_encoding = 10
-    # TODO check if fps % fps_encoding == 0. If not, output a warning
+    assert fps % fps_encoding == 0, "target fps for transcript features not a multiple of 10"
     multiplier = int(fps/fps_encoding)
+    print(f"multiplier: {multiplier}")
     for f in files:
         file = os.path.join(textpath, f + ".json")
         outfile = destpath + '/' + f + '.npy'
@@ -38,11 +39,21 @@ def extract_bert_features(textpath, files, destpath, fps):
                 file, tokenizer = embedding_model[0], bert_model = embedding_model[1])
         else:
             raise Exception('Something is wrong with the BERT embedding model')
-        # TODO Is this naive approach of doubling each entry correct? Try to understand G's approach
-        text_encoding = [val for val in text_encoding for _ in range(multiplier)]
-        text_encoding = np.array(text_encoding)
 
-        print(text_encoding.shape)
+        assert type(text_encoding) == np.ndarray, "Before duplication: Type of `text_encoding` wrong."
+
+        print(f"Shape before duplication: {text_encoding.shape}")
+        orig_dim_samples = text_encoding.shape[0]
+        cols = np.linspace(0, text_encoding.shape[0], dtype=int, endpoint=False, num=text_encoding.shape[0] * 2)
+        # NOTE: because of the dtype, 'cols' contains each index in 0:text.shape[0] twice
+        text_encoding = text_encoding[cols, :]
+        print(f"Shape after duplication: {text_encoding.shape}")
+
+        assert type(text_encoding) == np.ndarray, "After duplication: Type of `text_encoding` wrong."
+        # TODO adapt the following assertion to np.array (use shape) 
+        assert 2 * orig_dim_samples == text_encoding.shape[0], "BERT: Entry duplicating failed"
+
+        # print(text_encoding.shape)
         print(np.min(text_encoding),np.max(text_encoding))
         np.save(outfile, text_encoding)
 
